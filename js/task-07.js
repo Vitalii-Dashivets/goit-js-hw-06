4cf51e8bbc238b510f568e54c1360e562a3d1382
Vitalii-Dashivets <vdashivets1985@gmail.com>
2023-05-06 13:44:56 +0300

    Task 7 edited

--- js/task-07.js
+++ js/task-07.js
@@ -3,6 +3,12 @@ scale.addEventListener("input", setSizeText);
 
 const changeText = document.querySelector('#text');
 
+const minSize = scale.min;
+const maxSize = scale.max;
+const startSize = (maxSize - minSize) / 2;
+
+changeText.style.fontSize = `${startSize}px`;
+
 function setSizeText(event) {
     const size = event.currentTarget.value;
     changeText.style.fontSize = `${size}px`;
